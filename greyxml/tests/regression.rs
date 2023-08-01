81c0817f8486aa615b043a83e748307b1218baee
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2023-08-01 16:32:59 +0100

    test(greyxml): add regression tests for xml

---
+++ greyxml/tests/regression.rs
@@ -0,0 +1,34 @@
+use greyxml::{lex, tokenize};
+use std::fs::File;
+use std::io::Read;
+
+type TestResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn test_against_file(test_path: &str, test_output_path: &str) -> TestResult<()> {
+    let mut test_file = File::open(test_path)?;
+    let mut test_output = File::open(test_output_path)?;
+    let mut test_data = String::new();
+    let mut output = String::new();
+    test_file.read_to_string(&mut test_data)?;
+    test_output.read_to_string(&mut output)?;
+
+    let lexed = lex(test_data)?;
+    let mut iter = lexed.into_iter();
+    let tokens = tokenize(&mut iter)?;
+
+    assert_eq!(format!("{:?}", tokens), output);
+
+    Ok(())
+}
+
+#[test]
+fn rss() -> TestResult<()> {
+    test_against_file("./tests/test.rss", "./tests/test.rss.output")?;
+    Ok(())
+}
+
+#[test]
+fn html() -> TestResult<()> {
+    test_against_file("./tests/example.html", "./tests/example.html.output")?;
+    Ok(())
+}
