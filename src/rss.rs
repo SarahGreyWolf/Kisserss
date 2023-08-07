9aa988321c8a7f556e8c85ae664f6c2489991bde
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2023-08-07 18:13:32 +0100

    fix(rss): remove `(crate)` from all pub fields

--- src/rss.rs
+++ src/rss.rs
@@ -65,73 +65,73 @@ impl Feed {
 // https://www.rssboard.org/rss-specification
 #[derive(Default, Debug)]
 pub struct Channel {
-    pub(crate) title: Element<String>,
-    pub(crate) link: Element<String>,
-    pub(crate) description: Element<String>,
-    pub(crate) items: Vec<Element<Item>>,
-    pub(crate) language: Option<Element<String>>,
-    pub(crate) copyright: Option<Element<String>>,
-    pub(crate) managing_editor: Option<Element<String>>,
-    pub(crate) web_master: Option<Element<String>>,
+    pub title: Element<String>,
+    pub link: Element<String>,
+    pub description: Element<String>,
+    pub items: Vec<Element<Item>>,
+    pub language: Option<Element<String>>,
+    pub copyright: Option<Element<String>>,
+    pub managing_editor: Option<Element<String>>,
+    pub web_master: Option<Element<String>>,
     // TODO: Maybe make this a chrono time thing
-    pub(crate) pub_date: Option<Element<String>>,
-    pub(crate) last_build_date: Option<Element<String>>,
-    pub(crate) category: Option<Element<String>>,
-    pub(crate) generator: Option<Element<String>>,
-    pub(crate) docs: Option<Element<String>>,
-    pub(crate) cloud: Option<Element<String>>,
-    pub(crate) ttl: Option<Element<u32>>,
-    pub(crate) image: Option<Element<Image>>,
+    pub pub_date: Option<Element<String>>,
+    pub last_build_date: Option<Element<String>>,
+    pub category: Option<Element<String>>,
+    pub generator: Option<Element<String>>,
+    pub docs: Option<Element<String>>,
+    pub cloud: Option<Element<String>>,
+    pub ttl: Option<Element<u32>>,
+    pub image: Option<Element<Image>>,
     // FIXME: Idk what the format of this is yet
-    pub(crate) rating: Option<Element<String>>,
-    pub(crate) text_input: Option<Element<TextInput>>,
-    pub(crate) skip_hours: Option<Element<SkipHours>>,
-    pub(crate) skip_days: Option<Element<SkipDays>>,
+    pub rating: Option<Element<String>>,
+    pub text_input: Option<Element<TextInput>>,
+    pub skip_hours: Option<Element<SkipHours>>,
+    pub skip_days: Option<Element<SkipDays>>,
 }
 
 // https://www.rssboard.org/rss-specification#hrelementsOfLtitemgt
 #[derive(Default, Debug)]
 pub struct Item {
-    pub(crate) title: Option<Element<String>>,
-    pub(crate) link: Option<Element<String>>,
-    pub(crate) description: Option<Element<String>>,
-    pub(crate) author: Option<Element<String>>,
-    pub(crate) category: Option<Element<String>>,
-    pub(crate) enclosure: Option<Element<String>>,
-    pub(crate) guid: Option<Element<String>>,
-    pub(crate) pub_date: Option<Element<String>>,
-    pub(crate) source: Option<Element<String>>,
+    pub title: Option<Element<String>>,
+    pub link: Option<Element<String>>,
+    pub description: Option<Element<String>>,
+    pub author: Option<Element<String>>,
+    pub category: Option<Element<String>>,
+    pub enclosure: Option<Element<String>>,
+    pub guid: Option<Element<String>>,
+    pub pub_date: Option<Element<String>>,
+    pub source: Option<Element<String>>,
     // part of https://web.resource.org/rss/1.0/modules/content/
-    pub(crate) content_encoded: Option<Element<EncodedContent>>,
+    pub content_encoded: Option<Element<EncodedContent>>,
     // part of https://www.rssboard.org/media-rss
-    pub(crate) media: Option<Element<Media>>,
+    pub media: Option<Element<Media>>,
 }
 
 #[derive(Default, Debug)]
-pub struct EncodedContent(pub(crate) Vec<Element<String>>, pub(crate) String);
+pub struct EncodedContent(pub Vec<Element<String>>, pub(crate) String);
 
 #[derive(Default, Debug)]
 pub struct Image {
-    pub(crate) url: Element<String>,
-    pub(crate) title: Element<String>,
-    pub(crate) link: Element<String>,
-    pub(crate) width: Option<Element<u32>>,
-    pub(crate) height: Option<Element<u32>>,
-    pub(crate) description: Option<Element<String>>,
+    pub url: Element<String>,
+    pub title: Element<String>,
+    pub link: Element<String>,
+    pub width: Option<Element<u32>>,
+    pub height: Option<Element<u32>>,
+    pub description: Option<Element<String>>,
 }
 
 #[derive(Default, Debug)]
 pub struct TextInput {
-    pub(crate) title: Element<String>,
-    pub(crate) description: Element<String>,
-    pub(crate) name: Element<String>,
-    pub(crate) link: Element<String>,
+    pub title: Element<String>,
+    pub description: Element<String>,
+    pub name: Element<String>,
+    pub link: Element<String>,
 }
 
 #[derive(Default, Debug)]
 pub struct SkipHours {
     // A number between 0 and 23
-    pub(crate) hours: [u8; 24],
+    pub hours: [u8; 24],
 }
 
 #[derive(Default, Debug)]
@@ -148,7 +148,7 @@ pub enum Days {
 
 #[derive(Default, Debug)]
 pub struct SkipDays {
-    pub(crate) days: [Days; 7],
+    pub days: [Days; 7],
 }
 
 #[derive(Default, Debug)]
